9832ae6d932b33d6402617b2d92028984c599f60
Emma Ågren <agren.gastronomi@hotmail.com>
2026-02-19 17:14:28 +0100

    pulse-animation med js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './styles/main.scss';
+
+const cards = document.querySelectorAll('.card')
+
+cards.forEach(card => {
+    card.addEventListener('click' , () => {
+        card.classList.add('pulse')
+        setTimeout(() => {
+            card.classList.remove('pulse')
+        }, 600)
+    })
+})
